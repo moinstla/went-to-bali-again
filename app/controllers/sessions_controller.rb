56bb127d38e2e0b5749fec0af6fbbc7d7efa7d2a
Monique St. Laurent <moinstla@gmail.com>
2017-07-18 14:12:57 -0700

    add flash messages for signin and signout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
+      flash[:notice] = "You've successfully signed in!"
       session[:user_id] = user.id
       redirect_to '/'
     else
@@ -14,6 +15,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    flash[:notice] = "You've signed out."
     session[:user_id] = nil
     redirect_to '/sign_in'
   end
